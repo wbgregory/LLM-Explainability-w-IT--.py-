2b8f6fd31478e4ddb480143c320fd2a2b9b86e81
wbgregory <145291220+wbgregory@users.noreply.github.com>
2023-09-20 21:41:34 -0400

    Covering Array Verifier

---
+++ hello.py
@@ -0,0 +1,55 @@
+#created below is a covering array, along with another variable that contains value sets - the covering array is compared to the value sets in order to verify the presence of the value sets within the
+#covering array, to make sure that it is indeed a covering array.
+
+#additional notes - t = strength, k = columns, v = values within a specific row x column, N = rows.
+#.combinations generates column sets while .product generates value sets
+
+import itertools
+import random
+
+t = 3
+k = 3
+v = 3
+N = random.randint(1,100)
+
+ca_list = []
+for row in range(N):
+    covering_array = itertools.combinations(range(k), t)
+    ca_list.append(covering_array)
+
+#check_array = list(itertools.product(range(v), repeat=t))
+
+def verify_covering_array(ca):
+    verify = True
+    vals = []
+    for columns in itertools.combinations(range(k), t):
+        for values in itertools.product(range(v), repeat = t):
+            vals.append(values)
+            for row in ca:
+                if row in vals:
+                    continue
+                else:
+                    verify = False
+    return verify
+
+if verify_covering_array(ca_list):
+    f'Yay, a covering array!'
+else: 
+    f'There is an imposter among us!'
+
+
+
+
+
+    #for row in covering_array:
+        #output = True
+        #vals_in_row = tuple(row[col])
+        #for col in columns
+            #if col in vals_in_row:
+                #continue
+            #else:
+                #output = False
+                #break
+        #return output
+
+#verify_covering_array(ca_list)
